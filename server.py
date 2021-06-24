b31f3227d7289e7d8f0fe70a61e1e5a15382314c
Charlie@Cloudbreak <charlie@cloudbrk.io>
2021-06-23 20:43:08 -0400

    Implemented numeric keyboard popup on phones.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from flask_wtf.html5 import TelInput, DateInput
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.fields.core import FloatField, RadioField, SelectField
 from wtforms.validators import DataRequired
@@ -26,19 +27,19 @@ Bootstrap(app)
 # db.create_all()
 
 class WODWeightForm(FlaskForm):
-    one_rm = IntegerField("What is your 1RM?")
-    wod_reps = IntegerField("How many reps is your workout?")
+    one_rm = IntegerField("What is your 1RM?",widget=TelInput())
+    wod_reps = IntegerField("How many reps is your workout?",widget=TelInput())
     submit = SubmitField("Calculate WODWeight")
 
 class oneRMEForm(FlaskForm):
-    multirepload = IntegerField("How much weight did you lift?")
-    multirep = IntegerField("How many unbroken reps?")
+    multirepload = IntegerField("How much weight did you lift?",widget=TelInput())
+    multirep = IntegerField("How many unbroken reps?",widget=TelInput())
     submit = SubmitField("Calculate 1RME")
 
 class TargetWeightForm(FlaskForm):
     sex = RadioField(label="Gender",choices=[('m',"♂ Male"),('f',"♀ Female")],default='m')
-    age = IntegerField("Age (years)")
-    bw = FloatField("Body Weight (lb)")
+    age = IntegerField("Age (years)",widget=TelInput())
+    bw = FloatField("Body Weight (lb)",widget=TelInput())
     movement = SelectField(
         "Barbell Movement",
         choices=[
