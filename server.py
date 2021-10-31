142ed161573be36a05c2f7eca41fa8c77b25f43b
Charlie@Cloudbreak <charlie@cloudbrk.io>
2021-10-31 13:07:52 -0400

    iOS Splash Screens updated. Error 500 page added.

--- server.py
+++ server.py
@@ -536,6 +536,10 @@ def mobile():
 def page_not_found(e):
     return render_template("404.html"), 404
 
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("404.html"), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
